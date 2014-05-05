879df574e2e800218031a7feed37b7d799e0a602
Jiaming Li <Jiaming927@users.noreply.github.com>
2014-05-04 23:38:39 -0700

    Some comments

--- FileUpload/router.js
+++ FileUpload/router.js
@@ -2,9 +2,9 @@
 
 function route(handle, pathname, response, request) {
 	console.log("About to route a request for " + pathname);
-	if (typeof handle[pathname] === 'function') {
-		console.log("Routing you to " + pathname);
-		handle[pathname](response, request);
+	if (typeof handle[pathname] === 'function') { // If that's a function
+		console.log("Routing you to " + pathname); // For debug issues
+		handle[pathname](response, request); // Go to that function!
 	} else {
 		console.log("No request handler found for " + pathname);
 		response.writeHead(404, {"Content-Type": "text/plain"}); // Write directly
