6cfa3a0cf5fa283d35f87add730416e8b1121be8
Jiaming927 <masterjm@cs.washington.edu>
2014-05-10 02:32:33 -0700

    More functions
    Gotta sleep

--- NodeInAction/chatrooms/lib/chat_server.js
+++ NodeInAction/chatrooms/lib/chat_server.js
@@ -35,4 +35,66 @@ function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
     return guestNumber + 1;
 }
 
+// Handles joining room
+function joinRoom(socket, room) {
+    socket.join(room);
+    currentRoom[socket.id] = room;
+    socket.emit('joinResult', (room: room));
+    socket.broadcast.to(room).emit('message', {
+        text: nickNames[socket.id] + ' has joined ' + room + '.'
+    });
+
+    var usersInRoom = io.sockets.clients(room);
+    if (usersInRoom.length > 1) {
+        var usersInRoomSummary = 'Users currently in ' + room + ": ";
+        for (var index in usersInRoom) {
+            var userSocketId = usersInRoom[index].id;
+            if (userSocketId != socket.id) {
+                if (index > 0) {
+                    usersInRoomSummary += ', ';
+                }
+                userInRoomSummary += nickNames[userSocketId];
+            }
+        }
+        usersInRoomSummary += '.';
+        socket.emit('message', (text: usersInRoomSummary));
+    }
+}
+
+// Handles name changes
+
+function handleNameChangeAttempts(socket, nickNames, namesUsed) {
+    socket.on('nameAttempt', function(name) {
+        if (name.indexOf('Guest') == 0) {
+            socket.emit('nameResult', {
+                success: false,
+                message: 'Names cannot begin with "Guest".'
+            });
+        } else {
+            if (nameUsed.indexOf(name) == -1) {
+                var previousName = nickNames[socket.id];
+                var previousNameIndex = namesUsed.indexOf(previousName);
+                namesUsed.push(name);
+                nickNames[socket.id] = name;
+                delete namesUsed[previousNameIndex]; // wtf we can do this?
+                socket.emit(
+                    'nameResult',
+                    {
+                        success:true,
+                        name: name
+                    }
+                });
+                socket.broadcast.to(currentRoom[socket.id]).emit('message', {
+                    text: previousName + ' is now knowns as ' + name + '.'
+                });
+            } else {
+                socket.emit('nameResult', {
+                    success: false,
+                    message: 'That name is already in use bro.'
+                });
+            }
+        }
+    });
+}
+
 exports.listen = listen;
