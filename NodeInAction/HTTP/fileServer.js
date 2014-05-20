be7c2ea252538102ff00253e44e0fcebdd56eac6
Jiaming927 <masterjm@cs.washington.edu>
2014-05-19 18:14:51 -0700

    Uses stat to handle that

--- NodeInAction/HTTP/fileServer.js
+++ NodeInAction/HTTP/fileServer.js
@@ -8,12 +8,31 @@ var root = __dirname;
 var server = http.createServer(function(req, res) {
     var url = parse(req.url);
     var path = join(root, url.pathname);
-    var stream = fs.createReadStream(path);
-    stream.pipe(res);
-    stream.on('error', function(err) {
-        res.statusCode = 500;
-        res.end('Interal Server Error' + err);
+    fs.stat(path, function(err, stat) {
+        if(err) {
+            if ('ENOENT' == err.code) {
+                res.statusCode = 404;
+                res.end('Not Found');
+            } else {
+                res.statusCOde = 500;
+                res.end('Internal Server Error');
+            }
+        } else { // Uses stat function
+            res.setHeader('Content-Length'), stat.size;
+            var stream = fs.createReadStream(path);
+            stream.pipe(res);
+            stream.on('error', function(err) {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            });
+        }
     });
+    // var stream = fs.createReadStream(path);
+    // stream.pipe(res);
+    // stream.on('error', function(err) {
+    //     res.statusCode = 500;
+    //     res.end('Interal Server Error' + err);
+    // });
 });
 
 server.listen(8888);
