94e1ff6f315410670acf2110e69f6a57035b0462
Jiaming927 <masterjm@cs.washington.edu>
2014-05-22 23:50:49 -0700

    Mongoose...sexy....

---
+++ NodeInAction/storage/mongoose.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/tasks');
+
+var Schema = mongoose.Schema;
+var Tasks = new Schema({
+    project: String,
+    description: String
+});
+mongoose.model('Task', Tasks); // Register a schema
+
+var Task = mongoose.model('Task');
+var task = new Task();
+task.project = "Bikeshed";
+task.description = 'Paint the bikeshed red.';
+task.save(function(err) {
+    if (err) throw err;
+    console.log('Task saved.');
+});
