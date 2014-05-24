f727352b169ef5090d92fb195979a451b158d975
Jiaming927 <masterjm@cs.washington.edu>
2014-05-23 23:36:06 -0700

    Playing with mongo

--- NodeInAction/storage/mongoose.js
+++ NodeInAction/storage/mongoose.js
@@ -16,3 +16,10 @@ task.save(function(err) {
     if (err) throw err;
     console.log('Task saved.');
 });
+
+Task.find({'project': 'Bikeshed'}, function(err, tasks) {
+    for (var i = 0; i < tasks.length; i++) {
+        console.log('ID:' + tasks[i]._id);
+        console.log(tasks[i].description);
+    }
+})
