612591a1b07c4baf66f22b874869f13baa674ddf
Jiaming927 <masterjm@cs.washington.edu>
2014-05-26 23:43:37 -0700

    More middleware

--- NodeInAction/Connect/connect.js
+++ NodeInAction/Connect/connect.js
@@ -1,8 +1,13 @@
 var connect = require('connect');
-var app = connect();
-app.use(logger); // the order here is the order of middlewares
-app.use(hello);
-app.listen(8888);
+// var app = connect();
+// app.use(logger); // the order here is the order of middlewares
+// app.use(hello);
+// app.listen(8888);
+
+// Better way
+connect()
+    .use(logger)
+    .use(hello);
 
 function logger(req, res, next) { // next means next middleware
     console.log('%s %s', req.method, req.url);
@@ -12,4 +17,22 @@ function logger(req, res, next) { // next means next middleware
 function hello(req, res) { // No next needed because it responds here
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
+}
+
+// Restricted access
+function restrict(req, res, next) {
+    var authorization = req.headers.authorization; // Can only come from authorized area
+    if (!authorization) return next(new Error('Unauthorized'));
+    // Only error-handling middle will be executed
+
+    var parts = authorization.split(' ')
+    var scheme = parts[0]
+    var auth = new Buffer(parts[1], 'base64').toString().split(':')
+    var user = auth[0]
+    var pass = auth[1]
+
+    authenticateWithDatabase(user, pass, function (err) {
+        if (err) return next(err);
+        next();
+    })
 }
