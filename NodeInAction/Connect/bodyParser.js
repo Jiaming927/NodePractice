7833450c7ae2633d98d8124b666f192a50b81257
Jiaming927 <masterjm@cs.washington.edu>
2014-05-29 00:50:10 -0700

    Query middleware

--- NodeInAction/Connect/bodyParser.js
+++ NodeInAction/Connect/bodyParser.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
 var app = connect()
-    .use(connect.bodyParser())
+    .use(connect.bodyParser()) // Body parser doesn't care what's the content-type, request will tell it
     .use(function(req, res) {
         res.end('Registered new user: ' + req.body.username + "\n");
     }).listen(8888)
---
+++ NodeInAction/Connect/query.js
@@ -0,0 +1,8 @@
+// Handles GET request, while bodyParser handles POST, etc.
+var connect = require('connect');
+var app = connect()
+    .use(connect.query())
+    .use(function(req, res, next) {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(req.query));
+    }).listen(8888);
