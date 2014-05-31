d94f2e6aaa9a15acfd75f42ebc601fa501cbf010
Jiaming927 <masterjm@cs.washington.edu>
2014-05-31 02:12:44 -0700

    more auth function

--- NodeInAction/Connect/security.js
+++ NodeInAction/Connect/security.js
@@ -1,4 +1,22 @@
 var connect = require('connect');
 
+var users = {
+    tobi: 'foo',
+    loki: 'bar',
+    jane: 'baz'
+};
+
 var app = connect()
-    .use(connect.basicAuth('tj', 'tobi'));
+    //.use(connect.basicAuth('tj', 'tobi')); // Only one username and one password
+    // .use(connect.basicAuth(function(user, pass) { // Using a function to auth
+    //     return users[user] === pass;
+    // }));
+    .use(connect.basicAuth(function(user, pass, callback) {
+        User.authenticate({ user:user, pass:pass }, gotUser); // Asynchronous call back auth
+        function gotUser(err, user) {
+            if (err) return callback(err);
+            callback(null, user);
+        }
+    }))
+
+app.listen(8888);
