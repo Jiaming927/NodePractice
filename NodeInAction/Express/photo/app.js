627614bd48705fb0ebf591d0be4d6b97d270a9ea
Jiaming927 <masterjm@cs.washington.edu>
2014-06-03 00:34:08 -0700

    Weird thing went wrong
    use or get?

--- NodeInAction/Express/photo/app.js
+++ NodeInAction/Express/photo/app.js
@@ -22,10 +22,10 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', photos.list);
+//app.use('/users', users);
 
-app.get('/', photos.list);
+//app.get('/', photos.list);
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
