6d18c44a5d346a197397cd66fde9c23a33a409f8
Jiaming927 <masterjm@cs.washington.edu>
2014-06-23 23:34:39 +0800

    More buffer functions

--- NodeInAction/TCP/buffer.js
+++ NodeInAction/TCP/buffer.js
@@ -6,3 +6,16 @@ console.log(b);
 // <Buffer 31 32 31 32 33 34 38 36 39>
 
 // Buffer is like an array of raw data
+
+writeInt16LE(); // Smaller int
+
+writeUInt32LE(); // unsigned values
+
+writeInt32BE(); // big-endian values
+
+var c = new Buffer(4);
+c.writeUInt32LE();
+
+cnosole.log(c.length);
+
+console.log(b);
