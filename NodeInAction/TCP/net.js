63091e2f9e94d0c76f414414fc35eda2fff3f60c
Jiaming927 <masterjm@cs.washington.edu>
2014-06-25 10:35:13 +0800

    TCP server and client

--- NodeInAction/TCP/net.js
+++ NodeInAction/TCP/net.js
@@ -3,16 +3,21 @@ var net = require('net');
 net.createServer(function(socket) {
 	socket.write('Hello world');
 	socket.end();
+
+	console.log('listening on port 1337')
+
+	// Some examples
+
+	socket.on('data', function(data) {
+		console.log('got "data"', data);
+	});
+
+	socket.on('end', function() {
+		console.log('socket has ended');
+	});
+
+	socket.on('close', function() {
+		console.log('client disconnected');
+	});
+
 }).listen(1337);
-
-console.log('listening on port 1337')
-
-// Some examples
-
-socket.on('data', function(data) {
-	console.log('got "data"', data);
-});
-
-socket.on('end', function() {
-	console.log('socket has ended');
-});
